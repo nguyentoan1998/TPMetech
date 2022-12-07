d3e484c771cbda0ac1a8ae6ab1ac31d490d152c5
Admin <Admin@KETOAN>
2022-12-07 16:43:35 +0700

    Add project files.

---
+++ Database/Connecting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class Connecting
+    {
+        private SqlConnection OpenConnect()
+        {
+            SqlConnection sqlConnection;
+            string Connect = @"Data Source=DESKTOP-9UB1AIQ\TINPHAT;Initial Catalog=TinPhat;Persist Security Info=True;User ID=sa;Password=123456";
+            using (sqlConnection = new SqlConnection(Connect))
+            {
+                if (sqlConnection.State == System.Data.ConnectionState.Closed)
+                    sqlConnection.Open();
+            }
+            return sqlConnection;
+        }
+        public getlist()    
+    }
+}
